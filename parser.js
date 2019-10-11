3e4f7883a1b50e480d4eeea61290542222ade449
VK <vitaliy.krynytskyy@gmail.com>
2019-10-11 13:58:36 +0100

    Cleaned up the logic

--- parser.js
+++ parser.js
@@ -1,25 +1,24 @@
 import nightmare from 'nightmare';
 
-checkItem();
+const url = 'https://www.amazon.co.uk/Samsung-MZ-V7E1T0BW-V-NAND-Express-Solid/dp/B07CGJNLBB';
 
-async function checkItem() {
-
-    let item = await nightmare().goto(`https://www.amazon.co.uk/Samsung-MZ-V7E1T0BW-V-NAND-Express-Solid/dp/B07CGJNLBB`)
-        .wait(`#priceblock_ourprice, #productTitle`)
-        .evaluate(() => {
-            return {
-                title: document.getElementById(`productTitle`).innerText,
-                price: parseFloat(document.getElementById(`priceblock_ourprice`).innerText.replace('£', '')),
-                currency: document.getElementById(`priceblock_ourprice`).innerText.charAt(0)
-            }
-        })
-        .end();
-
-    if (item.price < 150) {
-        console.log(`Item: ${item.title} is currently on sale for ${item.price}`);
-    } else {
-        console.log(`Not cheap at the moment`);
-    }
-
-    console.log(item);
+const getItemByURL = async (url) => {
+    return new Promise(resolve => {
+        resolve(
+            nightmare().goto(url).wait(`#priceblock_ourprice, #productTitle`).evaluate(() => {
+                return {
+                    title: document.getElementById(`productTitle`).innerText,
+                    price: parseFloat(document.getElementById(`priceblock_ourprice`).innerText.replace('£', '')),
+                    currency: document.getElementById(`priceblock_ourprice`).innerText.charAt(0)
+                }
+            }).end());
+    });
 }
+
+const checkItemPrice = (item, threshold) => item.price < threshold ? console.log(`Item: ${item.title} is currently on sale for: ${item.price}`) : console.log(`Item: ${item.title} is not cheap enough at the moment.`);
+
+getItemByURL(url).then((item) => {
+    console.log(item);
+
+    checkItemPrice(item, 200);
+});
